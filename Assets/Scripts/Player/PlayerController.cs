e36278bda1df9142b79db4a3cceb210c249a14e2
Vic <viccjuan@gmail.com>
2024-03-21 13:29:15 -0500

    feat(player): Player Prefab
    The player prefab with its basic animations and configurations was added.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,28 +7,39 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRB;
+    private Animator playerAnimator;
     [SerializeField] private Transform refPie;
 
     [SerializeField] private float moveSpeed = 5f; // Velocidad de movimiento del personaje
 
     [SerializeField] private float jumpForce = 150;
     [SerializeField] private bool onFloor = false;
+    private bool lookAtRight = true;
+    private int velocityHash, YVelocityHash,onFloorHash;
 
     private void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
         refPie = GameObject.Find("Pie").gameObject.transform;
+
+        velocityHash = Animator.StringToHash("Velocity");
+        onFloorHash = Animator.StringToHash("onFloor");
+        YVelocityHash = Animator.StringToHash("YVelocity");
     }
 
     private void Update()
     {
         Move();
         Jump();
+        
+        playerAnimator.SetFloat(YVelocityHash, playerRB.velocity.y);
     }
 
     private void Jump()
     {
         onFloor = Physics2D.OverlapCircle(refPie.position, 1f, 1 << 3);
+        playerAnimator.SetBool(onFloorHash, onFloor);
         if (Input.GetButtonDown("Jump") && onFloor)
         {
             playerRB.AddForce(
@@ -41,14 +52,32 @@ public class PlayerController : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal");
 
+        if (moveInput > 0 && !lookAtRight)
+        {
+            Turn();
+        }
+        else if (moveInput < 0 && lookAtRight)
+        {
+            Turn();
+        }
+
         float moveVelocity = moveInput * moveSpeed;
+
+        playerAnimator.SetFloat(velocityHash, Math.Abs(moveInput));
 
         // Aplicar la velocidad al Rigidbody2D del personaje
         playerRB.velocity = new Vector2(moveVelocity, playerRB.velocity.y);
     }
 
+    private void Turn()
+    {
+        lookAtRight = !lookAtRight;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+
     private void Attack()
     {
-        
     }
 }
