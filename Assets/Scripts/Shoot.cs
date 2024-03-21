59ffbe0a6445b7cdbfad1185f89f039261517f9b
Vic <viccjuan@gmail.com>
2024-03-21 16:34:08 -0500

    feat(player): Player Shoot
    The player shooting was created.

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,41 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float bulletSpeed = 100f;
+    private Animator playerAnimator;
 
-    void Update()
+    private int isWeaponRaisedHash;
+    public float bulletSpeed = 100f;
+    private float weaponRaisedTime = 3.0f;
+
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0))
+        playerAnimator = GetComponentInParent<Animator>();
+        isWeaponRaisedHash = Animator.StringToHash("isWeaponRaised");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
             // Calcula la posición de inicio de la bala como la posición actual del jugador
             Vector3 bulletStartPosition = transform.position;
-            
+
             // Instancia la bala en la posición calculada con la rotación del jugador
             GameObject bullet = Instantiate(bulletPrefab, bulletStartPosition, transform.rotation);
-            
+            playerAnimator.SetBool(isWeaponRaisedHash, true);
+
             // Accede al componente Rigidbody de la bala y le aplica una velocidad en la dirección hacia adelante del jugador
-            bullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed;
+            Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+            bulletRB.velocity = transform.right * bulletSpeed;
+
+            StartCoroutine(LowerWeaponAfterDelay());
         }
+    }
+
+    private IEnumerator LowerWeaponAfterDelay()
+    {
+        yield return new WaitForSeconds(weaponRaisedTime);
+
+        playerAnimator.SetBool(isWeaponRaisedHash, false);
     }
 }
