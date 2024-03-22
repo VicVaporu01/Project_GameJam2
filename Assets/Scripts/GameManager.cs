4ce4e714895ef644274d184d0351425161b56f15
flejrat <santid99@gmail.com>
2024-03-22 14:41:46 -0500

    Commit de GameManager
    Se añadio el GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,24 @@ using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
 
+    PlayerController player;
+    TextMeshProUGUI Balas;
+
+    public static GameManager Instance {get; private set;}
+
+    private void Awake()
+    {
+        if (Instance != null && Instance == this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +36,9 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void cambioDeEscena(string Escena)
+    {
+        SceneManager.LoadScene(Escena);
     }
 }
