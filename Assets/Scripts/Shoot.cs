59408020e8b56a5380e59d587c529ae99b80ad17
Vic <viccjuan@gmail.com>
2024-03-22 13:22:39 -0500

    fix(player): Player Shoot and Bullet
    The bug with the direction of the bullet when the player was shooting was fixed, and the animations were added to the player standing shooting and the bullet hit.

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject bulletPrefab;
     private Animator playerAnimator;
 
     private int isWeaponRaisedHash;
-    public float bulletSpeed = 100f;
+    public float bulletSpeed = 25.0f;
     private float weaponRaisedTime = 3.0f;
 
     private void Start()
@@ -22,19 +21,36 @@ public class Shoot : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            // Calcula la posición de inicio de la bala como la posición actual del jugador
-            Vector3 bulletStartPosition = transform.position;
-
-            // Instancia la bala en la posición calculada con la rotación del jugador
-            GameObject bullet = Instantiate(bulletPrefab, bulletStartPosition, transform.rotation);
-            playerAnimator.SetBool(isWeaponRaisedHash, true);
-
-            // Accede al componente Rigidbody de la bala y le aplica una velocidad en la dirección hacia adelante del jugador
-            Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
-            bulletRB.velocity = transform.right * bulletSpeed;
-
-            StartCoroutine(LowerWeaponAfterDelay());
+            ShootBullet();
         }
+    }
+
+    private void ShootBullet()
+    {
+        // Calcula la posición de inicio de la bala como la posición actual del jugador
+        Vector3 bulletStartPosition = transform.position;
+
+        // Instancia la bala en la posición calculada con la rotación del jugador
+        GameObject bullet = BulletPool.Instance.RequestBullet();
+        bullet.transform.position = bulletStartPosition;
+        bullet.transform.rotation = transform.rotation;
+
+        playerAnimator.SetBool(isWeaponRaisedHash, true);
+
+        // Accede al componente Rigidbody de la bala y le aplica una velocidad en la dirección hacia adelante del jugador
+        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+        bulletRB.velocity = transform.right * bulletSpeed;
+
+        if (this.transform.parent.transform.localScale.x < 0)
+        {
+            bulletRB.velocity *= -1;
+        }
+        else
+        {
+            bulletRB.velocity *= 1;
+        }
+
+        StartCoroutine(LowerWeaponAfterDelay());
     }
 
     private IEnumerator LowerWeaponAfterDelay()
